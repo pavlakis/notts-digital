69c38799962a4b8a72e35c4df58ee2aee906d914
Antonios Pavlakis <antonis@pavlakis.info>
2017-01-29 21:34:54 +0000

    Change visibility and add try/catch around request

--- app/src/Http/Request/MeetupRequest.php
+++ app/src/Http/Request/MeetupRequest.php
@@ -3,64 +3,128 @@
  * Nottingham Digital events
  *
  * @link      https://github.com/pavlakis/notts-digital
- * @copyright Copyright (c) 2016 Antonios Pavlakis
+ * @copyright Copyright (c) 2017 Antonios Pavlakis
  * @license   https://github.com/pavlakis/notts-digital/blob/master/LICENSE (BSD 3-Clause License)
  */
 
-namespace NottsDigital\Http;
+namespace NottsDigital\Http\Request;
 
 
 use GuzzleHttp\Client,
-    Doctrine\Common\Cache\FilesystemCache;
+    NottsDigital\Cache\Cache;
 
-final class Request
+/**
+ * Class MeetupRequest
+ * @package NottsDigital\Http\Request
+ */
+final class MeetupRequest
 {
-    public function __construct(Client $httpClient, FilesystemCache $cache)
-    {
+    /**
+     * @var Client
+     */
+    private $httpClient;
 
+    /**
+     * @var Cache
+     */
+    private $cache;
+
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * @var string
+     */
+    private $baseUrl;
+
+    /**
+     * @var array
+     */
+    private $uris;
+
+    /**
+     * MeetupRequest constructor.
+     * @param Client $httpClient
+     * @param Cache $cache
+     * @param $apiKey
+     * @param $baseUrl
+     * @param $uris
+     * @param $config
+     */
+    public function __construct(
+        Client $httpClient,
+        Cache $cache,
+        $apiKey,
+        $baseUrl,
+        $uris,
+        $config
+    )
+    {
+        $this->uris = $uris;
+        $this->cache = $cache;
+        $this->config = $config;
+        $this->apiKey = $apiKey;
+        $this->baseUrl = $baseUrl;
+        $this->httpClient = $httpClient;
     }
 
-    protected function fetchEventInfo($groupUrlName)
+    /**
+     * @param $groupUrlName
+     * @return array
+     */
+    public function fetchEventInfo($groupUrlName)
     {
         // if cached, return cache
-        if ($this->isCached($groupUrlName . '_event-info')) {
+        $cacheId = $groupUrlName . '_event-info';
+
+        if (!$this->cache->contains($cacheId)) {
+
+            try {
+
+                $response = $this->httpClient->get($this->baseUrl . sprintf($this->uris['events'], $groupUrlName, $this->apiKey));
+                $this->cache->save($cacheId, $response->getBody()->getContents());
+            } catch (\Exception $e) {
+                // todo - add logging
+            }
 
         }
 
-        $response = $this->client->get($this->baseUrl . sprintf($this->uris['events'], $groupUrlName, $this->apiKey));
-        $events = json_decode($response->getBody()->getContents(), true);
+        $jsonResponse = $this->cache->fetch($cacheId);
+        $events = json_decode($jsonResponse, true);
 
         return $events;
     }
 
-    protected function fetchGroupInfo($groupUrlName)
+    /**
+     * @param $groupUrlName
+     * @return array
+     */
+    public function fetchGroupInfo($groupUrlName)
     {
         // if cached, return cache
-        if ($this->isCached($groupUrlName . '_group-info')) {
+        $cacheId = $groupUrlName . '_group-info';
+        if (!$this->cache->contains($cacheId)) {
 
+            try {
+
+                $response = $this->httpClient->get($this->baseUrl . sprintf($this->uris['groups'], $groupUrlName, $this->apiKey));
+                $this->cache->save($cacheId, $response->getBody()->getContents());
+            } catch (\Exception $e) {
+                // todo - add logging
+            }
         }
 
-        $response = $this->client->get($this->baseUrl . sprintf($this->uris['groups'], $groupUrlName, $this->apiKey));
+        $jsonResponse = $this->cache->fetch($cacheId);
 
-        $groupInfo = json_decode($response->getBody()->getContents(), true);
+        $groupInfo = json_decode($jsonResponse, true);
 
         return $groupInfo;
     }
-
-    protected function isCached($group)
-    {
-        // check if available and not expired
-        return $this->cache->contains($group);
-    }
-
-    protected function getFromCache($group)
-    {
-        return $this->cache->fetch($group);
-    }
-
-    protected function cache($group, $expires)
-    {
-
-    }
-
 }
