233fcadadd0411174d4973ed8d0ec272fd499efd
Shaun Hare <shaun@shaunhare.co.uk>
2017-01-30 11:04:51 +0000

    Develop (#25)
    * Adding TEch on toast group , hardening and testign name matching
    
    * Checking if event still comes under main group
    
    * Altering to cater for tech nottingham multiple events
    
    *  fixing up MeetupAdapterTests following refactor - match tests using Guzzle Mockhander also have cache mocked out and final removed from cache

--- tests/phpunit/Adapter/MeetupAdapterTest.php
+++ tests/phpunit/Adapter/MeetupAdapterTest.php
@@ -8,10 +8,16 @@
  */
 namespace NottsDigital\Tests\Adapter;
 
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 use NottsDigital\Adapter\MeetupAdapter;
 use GuzzleHttp\Client;
 use NottsDigital\Event\EventEntityCollection;
 use NottsDigital\Http\Request\MeetupRequest;
+use NottsDigital\Cache\Cache;
 
 class MeetupAdapterTest extends \PHPUnit_Framework_TestCase
 {
@@ -115,5 +121,63 @@ class MeetupAdapterTest extends \PHPUnit_Framework_TestCase
 
         $this->assertTrue($meetupAdapter->getEventEntityCollection()[0]->getTitle() === 'Industrial Control Cyber Security');
         $this->assertTrue($meetupAdapter->getEventEntityCollection()[1]->getTitle() === 'Current Postgraudate Research');
+    }
+
+
+    public function testFetchWillMatchNameCorrectly()
+    {
+    
+        $mock = new MockHandler([
+            new Response(200, [],file_get_contents(__DIR__ . '/feeders/nameMatch.json'))
+        ]);
+    
+        $handler = HandlerStack::create($mock);
+        $httpClient = new Client(['handler' => $handler]);
+        $mockedCache = $this->getMockBuilder(Cache::class);
+        $mockedCache = $mockedCache->disableOriginalConstructor()->getMock();
+        $mockedCache->method('fetch')
+             ->willReturn(file_get_contents(__DIR__ . '/feeders/nameMatch.json'));
+        $meetupRequest = new MeetupRequest($httpClient,$mockedCache,$this->apiKey, $this->baseUrl, $this->config['meetups']['uris'],$this->config);
+
+        $meetupAdapter = new MeetupAdapter(
+            $this->config['meetups'],
+            $meetupRequest,
+            new EventEntityCollection()
+        );
+        
+        $meetupAdapter->fetch('Tech on Toast');
+       
+        $this->assertNotNull($meetupAdapter->getEventEntityCollection());
+        $this->assertEquals($meetupAdapter->getEventEntityCollection()[0]->getTitle(), 'Tech on Toast January 2017 - The Launch!');
+    }
+    
+    
+    
+    public function testFetchWillStillGetEventAsPartOfMainGroup()
+    {
+        
+        $mock = new MockHandler([
+            new Response(200, [],file_get_contents(__DIR__ . '/feeders/nameMatch.json'))
+        ]);
+        
+        $handler = HandlerStack::create($mock);
+        $httpClient = new Client(['handler' => $handler]);
+        $mockedCache = $this->getMockBuilder(Cache::class);
+        $mockedCache = $mockedCache->disableOriginalConstructor()->getMock();
+        $mockedCache->method('fetch')
+             ->willReturn(file_get_contents(__DIR__ . '/feeders/nameMatch.json'));
+       
+        $meetupRequest = new MeetupRequest($httpClient,$mockedCache,$this->apiKey, $this->baseUrl, $this->config['meetups']['uris'],$this->config);
+
+        $meetupAdapter = new MeetupAdapter(
+            $this->config['meetups'],
+            $meetupRequest,
+            new EventEntityCollection()
+        );
+        
+        $meetupAdapter->fetch('Tech Nottingham');
+        
+        $this->assertNotNull($meetupAdapter->getEventEntityCollection());
+        $this->assertEquals(count($meetupAdapter->getEventEntityCollection()), 1);
     }
 }
