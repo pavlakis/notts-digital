9a8b1003bd05b74b7aa347211926b099f2767f87
Antonios Pavlakis <antonios.pavlakis@staysafeapp.com>
2019-08-13 16:33:51 +0100

    Pass all dependencies in the EventFactory

--- app/src/Event/EventFactory.php
+++ app/src/Event/EventFactory.php
@@ -2,23 +2,43 @@
 
 namespace NottsDigital\Event;
 
-use Pimple\Container;
+use NottsDigital\Adapter\MeetupAdapter;
+use NottsDigital\Adapter\TitoAdapter;
 
 class EventFactory
 {
     /**
-     * @param string    $eventType
-     * @param Container $container
-     * @return EventInterface
-     *
-     * @throws \InvalidArgumentException
+     * @var MeetupAdapter
      */
-    public static function createFromEventType(string $eventType, Container $container): EventInterface
+    private $meetupAdapter;
+
+    /**
+     * @var TitoAdapter
+     */
+    private $titoAdapter;
+
+    public function __construct(MeetupAdapter $meetupAdapter, TitoAdapter $titoAdapter)
+    {
+        $this->meetupAdapter = $meetupAdapter;
+        $this->titoAdapter = $titoAdapter;
+    }
+
+    /**
+     * @param string $eventType|null
+     * @return EventInterface
+     */
+    public function createFromEventType(?string $eventType): EventInterface
     {
         if (!in_array($eventType, ['ti.to', 'meetups'])) {
             throw new \InvalidArgumentException(sprintf('Event [%s] does not exist', $eventType));
         }
 
-        return $container['event.' . $eventType];
+        if ('meetups' === $eventType) {
+            return new Event($this->meetupAdapter);
+        }
+
+        if ('ti.to' === $eventType) {
+            return new Event($this->titoAdapter);
+        }
     }
 }
